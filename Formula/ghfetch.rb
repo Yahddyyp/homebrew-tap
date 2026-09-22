39ed46b3f9722adda181e2975254c070d2ba114f
Yahddyyp <Yahddyyp@gmail.com>
2026-09-22 20:29:15 +0530

    Update for new release of ghfetch

--- Formula/ghfetch.rb
+++ Formula/ghfetch.rb
@@ -1,8 +1,8 @@
 class Ghfetch < Formula
   desc "Display your github status in a beautiful way"
   homepage "https://github.com/Yahddyyp/gh-fetch"
-  url "https://github.com/Yahddyyp/ghfetch/releases/download/v0.1.0/ghfetch-v1.0.0-aarch64-apple-darwin.tar.gz"
-  sha256 "9589b8914beb637c485dd363a75dcdf7fb3c764812ee1db54a3b6b39728a8ed5"
+  url "https://github.com/Yahddyyp/ghfetch/releases/download/v1.0.1/ghfetch-v1.0.1-aarch64-apple-darwin.tar.gz"
+  sha256 "56ffa9be0a19b4df5e199c8f5f865128ec12eefe64464be93b94f25d7d6fe4c2"
   version "1.0.0"
 
   def install
