5266cc27b0a181465440b089856338d6b7363a2a
Yahddyyp <Yahddyyp@gmail.com>
2026-09-23 19:59:08 +0530

    Fix up tap for the casks and formulae

--- Formula/pass-tomb.rb
+++ Formula/pass-tomb.rb
@@ -1,24 +1,28 @@
 class PassTomb < Formula
-  desc "The pass-tomb extension for macos"
+  desc "The pass-tomb extension for macOS"
   homepage "https://github.com/Yahddyyp/pass-tomb-osx"
+  version "1.0.0"
   url "https://github.com/Yahddyyp/pass-tomb-osx.git",
-      tag: "v1.0.0"
+    tag: "v#{version}"
   license "MIT"
   head "https://github.com/Yahddyyp/pass-tomb-osx.git", branch: "main"
 
-  depends_on "rust" => :build
-  depends_on "pass"
   depends_on "gnupg"
+  depends_on "pass"
+  depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
 
     (share/"pass-extensions").mkpath
+
     %w[tomb open close timer].each do |cmd|
       (share/"pass-extensions/#{cmd}.bash").write <<~EOS
         #!/bin/bash
+
         exec "#{bin}/pass-#{cmd}" "$@"
       EOS
+
       chmod 0755, share/"pass-extensions/#{cmd}.bash"
     end
   end
